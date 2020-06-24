8d47d696165e6af7d48bc414be65af5e8120e444
Cesar Morales <cesarlenin2@live.com>
2020-06-24 07:28:40 -0700

    3.1 passing

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -18,6 +18,19 @@ class LearningRoute extends Component {
     return (
       <section>
         implement and style me
+        <h2>Translate the word:</h2>
+        <span>{this.context.head.nextWord}</span>
+        <p>Your total score is: {this.context.head.totalScore}</p>
+        <div>
+          <p>You have answered this word correctly {this.context.head.wordCorrectCount} times.</p>
+          <p>You have answered this word incorrectly {this.context.head.wordIncorrectCount} times.</p>
+        </div>
+        <form>
+          <label for='learn-guess-input'>What's the translation for this word?</label>
+          <input type='text' className='learn-guess-input' id='learn-guess-input' name='learn-guess-input' required></input>
+          <button type='submit'>Submit your answer</button>
+
+        </form>
       </section>
     );
   }
